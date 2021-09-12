4953d370f6fdab940699e89930c80a8876a205f9
Ludvig Magnusson <ludvig.magnusson04@gmail.com>
2021-09-12 20:52:10 +0200

    fix null voice channel

--- commands/leave.js
+++ commands/leave.js
@@ -3,9 +3,6 @@ const playToVoiceChannel = require("../playToVoiceChannel");
 const Queue = require("../Queue")
 
 module.exports = (message, args) => {
-    const voiceChannel = message.member.voice.channel;
-    voiceChannel.leave();
-
     if (playToVoiceChannel.voiceChannel())
         playToVoiceChannel.voiceChannel().leave();
     else {
